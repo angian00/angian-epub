6c933062e7692e88078d289e91a920ae21d7cd25
Andrea Giancola <andrea.giancola@telecomitalia.it>
2019-12-16 12:55:58 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,172 @@
+const { app, BrowserWindow, Menu, dialog } = require('electron');
+
+const fs = require('fs');
+const xml2json = require('xml2json');
+const extractZip = require('extract-zip');
+
+
+const contentPath = 'OEBPS/content.opf';
+const tocPath = 'OEBPS/toc.ncx';
+const textPath = 'OEBPS/Text/Section0049.htm';
+
+
+function createWindow() {
+	let win = new BrowserWindow({
+		width: 1200,
+		height: 900,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+
+	win.loadFile('index.html');
+	//win.webContents.openDevTools();
+
+	setupMenu(win);
+}
+
+app.on('ready', createWindow);
+
+
+function setupMenu(win) {
+	let menu = Menu.buildFromTemplate([
+		{
+			label: 'File',
+			submenu: [
+				{ label: 'Open File...', accelerator: 'Ctrl+O', 
+					click() { 
+						dialog.showOpenDialog({
+							title: "Choose an .epub file to open",
+							defaultPath: ".",
+							properties: ['openFile'],
+							filters: [
+								{ name: 'epubs', extensions: ['epub'] },
+								{ name: 'All Files', extensions: ['*'] }
+							],
+						}).then(result => {
+							if ((!result.canceled) && (result.filePaths.length > 0)) {
+								loadEpub(result.filePaths[0], (epubDir) => { parseEpub(epubDir, win) });
+							}
+						});
+					}
+				},
+
+				{ type:  'separator' },
+
+				{ label: 'Quit', accelerator: 'Ctrl+Q', click() { app.quit() } }
+			]
+		}
+	]);
+
+	Menu.setApplicationMenu(menu);
+}
+
+
+function loadEpub(epubPath, callback) {
+
+	//let tmpName = "testUnzip";
+	let epubName = "testUnzip";
+	let cacheDir = app.getPath("userData") + "/epubCache/" + epubName;
+
+	console.log("loading epub: " + epubPath + " to cacheDir: " + cacheDir);
+
+	extractZip(epubPath, {dir: cacheDir}, function (err) {
+		if (err) {
+			//TODO: manage unzip errors
+			console.log(err);
+		} else {
+			callback(cacheDir);
+		}
+	});
+
+	//TODO: get epub metadata and rename cache dir accordingly
+}
+
+function parseEpub(epubDir, win) {
+	parseMetadata(epubDir + "/"  + contentPath, 
+		(metadata) => { win.webContents.send('update-metadata', metadata); });
+	
+	parseToc(epubDir + "/"  + tocPath, 
+		(toc) => { win.webContents.send('update-toc', toc); });
+	
+	parseText(epubDir + "/"  + textPath, 
+		(text) => { win.webContents.send('update-text', text); });
+}
+
+function parseMetadata(contentFile, callback) {
+	fs.readFile(contentFile, function(err, data) {
+		let parsedMetadata = {};
+
+		let json = xml2json.toJson(data, {object: true});
+		let origMetadata = json.package.metadata;
+
+		for (let tag in origMetadata) {
+			if (tag.startsWith("xmlns") || tag.startsWith("meta"))
+				continue;
+
+			let value = origMetadata[tag];
+			//console.log("tag: " + tag + "; value type: " + (typeof value));
+
+			if (tag == "dc:date") {
+				for (let subItem of value) {
+					let tagNew = tag + "_" + subItem["opf:event"];
+					let valueNew = subItem["$t"];
+					parsedMetadata[tagNew] = valueNew;
+				}
+
+			} else if (typeof value == "object") {
+				value = value["$t"];
+				parsedMetadata[tag] = value;
+
+			} else {
+				parsedMetadata[tag] = value;
+			}
+
+		}
+
+		callback(parsedMetadata);
+	});
+}
+
+
+function parseToc(tocFile, callback) {
+	fs.readFile(tocFile, function(err, data) {
+		let parsedToc = [];
+
+		let json = xml2json.toJson(data, {object: true});
+		let navMap = json.ncx.navMap;
+
+		for (let navPoint of navMap.navPoint) {
+			//console.log("\t" + navPoint.navLabel.text);
+
+			section = {title: navPoint.navLabel.text, url: navPoint.content.src, subsections: []};
+			let subItems = navPoint.navPoint;
+			if (!subItems)
+				subItems = [];
+
+			else if (!Array.isArray(subItems))
+				subItems = [subItems];
+
+			for (let navPoint2 of subItems) {
+				//console.log("\t\t" + navPoint2.navLabel.text);
+				section.subsections.push({title: navPoint2.navLabel.text, url: navPoint2.content.src, subsections: []});
+			}
+
+			parsedToc.push(section);
+		}
+
+		callback(parsedToc);
+	});
+}
+
+
+function parseText(textFile, callback) {
+	fs.readFile(textFile, function(err, data) {
+		const jsdom = require("jsdom");
+		const xmlDoc = new jsdom.JSDOM(data);
+
+		let htmlText = xmlDoc.window.document.querySelector("body").innerHTML;
+		
+		callback(htmlText);
+	});
+}
---
+++ view.js
@@ -0,0 +1,62 @@
+const { ipcRenderer } = require('electron');
+
+ipcRenderer.on('update-metadata', function(event, metadata) {
+	//console.log("update metadata");
+	let container = document.getElementById("metadataContainer");
+
+	let list = container.appendChild(document.createElement("dl"));
+	for (let k in metadata) {
+		let dt = document.createElement("dt");
+		dt.appendChild(document.createTextNode(k));
+		list.appendChild(dt);
+		let dd = document.createElement("dd");
+		dd.appendChild(document.createTextNode(metadata[k]));
+		list.appendChild(dd);
+	}
+});
+
+
+ipcRenderer.on('update-toc', function(event, toc) {
+	//console.log("update toc");
+	let container = document.getElementById("tocContainer");
+
+	let list = container.appendChild(document.createElement("ul"));
+	for (let section of toc) {
+		let li = document.createElement("li");
+		li.appendChild(linkOrText(section.title, section.url));
+		list.appendChild(li);
+
+		if ((section.subsections) && (section.subsections.length > 0)) {
+			let sublist = li.appendChild(document.createElement("ul"));
+			for (let subsection of section.subsections) {
+				let li2 = document.createElement("li");
+				li2.appendChild(linkOrText(subsection.title, subsection.url));
+				sublist.appendChild(li2);
+			}
+		}
+	}
+});
+
+
+ipcRenderer.on('update-text', function(event, htmlText) {
+	//console.log("update text");
+	let container = document.getElementById("textContainer");
+	container.innerHTML = htmlText;
+});
+
+
+function linkOrText(text, targetUrl) {
+	let elem = null;
+
+	if (targetUrl && (targetUrl != "")) {
+		elem = document.createElement('a');  
+		let linkText = document.createTextNode(text);
+		elem.appendChild(linkText);  
+		elem.href = targetUrl;
+
+	} else {
+		elem = document.createTextNode(text);
+	}
+
+	return elem;
+}
